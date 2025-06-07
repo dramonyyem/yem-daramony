81537c9c0f0839a3e2d3c2bca5778fb6b10cb981
daramonyyem <yem.daramony@gmail.com>
2025-06-07 22:30:10 +0700

    second commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ContactView from '@/views/ContactView.vue'
+import TestingView from '@/views/TestingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,6 +23,11 @@ const router = createRouter({
       path: '/contact',
       name: 'contact',
       component: ContactView,
+    },
+    {
+      path: '/testing',
+      name: 'testing',
+      component: TestingView,
     }
   ],
 })
