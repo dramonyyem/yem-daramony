c4b770e06d2d69ec6c278132e882d17b9885e198
daramonyyem <yem.daramony@gmail.com>
2025-06-10 18:26:16 +0700

    eightth commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import Modal from './components/Modal.vue';
+
 
 // Rotation toggle state
 const isRotated = ref(false);
+const showModal = ref(false);
 
 const toggleTransform = () => {
   isRotated.value = !isRotated.value;
+};
+const displayModal = () => {
+  showModal.value = true;
 };
 </script>
 
@@ -36,7 +42,7 @@ const toggleTransform = () => {
             </div>
             <div class="h-[80px]"></div>
             <div>
-              <button class="px-4 py-4 bg-gray-200 rounded-lg text-left">
+              <button @click="displayModal" class="px-4 py-4 bg-gray-200 rounded-lg text-left">
                 <div class="flex items-center gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
                     <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
@@ -220,6 +226,11 @@ const toggleTransform = () => {
       </div>
     </div>
   </main>
+  <Modal v-model="showModal">
+    <h2>Facebook</h2>
+    <h2>Linkdein</h2>
+    <h2>github</h2>
+  </Modal>
 </template>
 
 <style scoped>
